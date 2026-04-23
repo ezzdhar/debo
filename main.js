002815a58b2cc316a8f844e73baf0f516bf1442c
sara-gooda <zaragooda925@gmail.com>
2026-04-23 04:09:12 -0700

    first edit home page

--- main.js
+++ main.js
@@ -255,10 +255,10 @@ window.addEventListener('scroll', () => {
     const header = document.querySelector('header');
     if (window.scrollY > 50) {
         header.style.padding = '15px 5%';
-        header.style.background = 'rgba(0, 0, 0, 0.95)';
+        header.style.background = '#020202';
     } else {
         header.style.padding = '20px 5%';
-        header.style.background = 'rgba(0, 0, 0, 0.8)';
+        header.style.background = '#020202';
     }
 });
 
